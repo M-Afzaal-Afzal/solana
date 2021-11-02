c30f2def44b05fbe710d8b2f693861e87440f359
M Afzaal Afzal <afzaal1721@gmail.com>
2021-11-02 18:35:09 +0500

    bug fixed

--- components/Roadmap.jsx
+++ components/Roadmap.jsx
@@ -7,49 +7,49 @@ const RoadmapData=[
     {
         id: 1,
         title:"10%",
-        url:"pics/roadmap/egg 1.png",
+        url:"/pics/roadmap/egg 1.png",
         query:"Website Launch",
         answer:"Website is live and preparing for the Dino day."
     },
     {
         id: 2,
         title:"20%",
-        url:"pics/roadmap/egg 2.png",
+        url:"/pics/roadmap/egg 2.png",
         query:"Pre-launch giveaways",
         answer:"We will announce at least 50 giveaways. We will pick random participants from our Discord & Twitter and reward them with their own Baby Dino on the launch day. Any memeber of our community can be the lucky winner!"
     },
     {
         id: 3,
         title:"40%",
-        url:"pics/roadmap/egg 3.png",
+        url:"/pics/roadmap/egg 3.png",
         query:"Launch & Minting",
         answer:"All of 10.000 Baby Dinos will be released on our website to the community in a fair minting process"
     },
     {
         id: 4,
         title:"50%",
-        url:"pics/roadmap/egg 4.png",
+        url:"/pics/roadmap/egg 4.png",
         query:"Marketplace Integration",
         answer:"You can buy or sell your Baby Dinos on Solsea after the launch."
     },
     {
         id: 5,
         title:"60%",
-        url:"pics/roadmap/egg 5.png",
+        url:"/pics/roadmap/egg 5.png",
         query:"Buyback",
         answer:"After 50% of the Dinos are minted, we will buy back Dinos worth 200 SOL to speed things up and protect floor price."
     },
     {
         id: 6,
         title:"70%",
-        url:"pics/roadmap/egg 6.png",
+        url:"/pics/roadmap/egg 6.png",
         query:"Buyback II",
         answer:"After 90% of Dinos are minted, we will buy back Dinos worth 300 SOL to protect floor price."
     },
     {
         id: 7,
         title:"80%",
-        url:"pics/roadmap/egg 7.png",
+        url:"/pics/roadmap/egg 7.png",
         query:"Royalties Sharing",
         answer:"After 100% of the Dinos are minted, we will start sharing the royalties with our hodlers. 20 % of the royalties will be distributed among all the wallets holding at least 5 Dinos. Distribution will function on weekly basis."
     },
@@ -57,13 +57,13 @@ const RoadmapData=[
         id: 8,
         title:"90%",
         query:"Tree planting",
-        url:"pics/roadmap/egg 8.png",
+        url:"/pics/roadmap/egg 8.png",
         answer:"After all the Dinos are sold, we will plant 10.000 new trees. One tree for each Dino. Because Dinos need a new home and our planet needs more trees. "
     },
     {
         id: 9,
         title:"100%",
-        url:"pics/roadmap/egg 9.png",
+        url:"/pics/roadmap/egg 9.png",
         query:"Big Dinos coming",
         answer:"Time for the Big Dinos! Because the Dinos not only need a new home, but they also need family. So a new collection will be introduced and airdroped to our hodlers. Any wallet holding at least 5 Dinos will receive a free NFT of Big Dino."
     }
--- components/RoadmapContent.jsx
+++ components/RoadmapContent.jsx
@@ -1,86 +1,95 @@
-import { Box } from "@mui/system";
+import {Box} from "@mui/system";
 import Typography from "@mui/material/Typography";
 
 function RoadmapContent(props) {
     return (
         <Box
-          key={props.id}
-          className="roadmap-content"
-          component="div"
-          sx={{
-            mt: 4,
-          }}
-        >
-          <Box
-            className="step"
+            key={props.id}
+            className="roadmap-content"
             component="div"
             sx={{
-              display: "flex",
-              justifyContent: "flex-start",
-              alignItems: "center",
+                mt: 4,
             }}
-          >
+        >
             <Box
-              className="content-left"
-              component="div"
-              sx={{ width: "20%", position: "relative" }}
-            >
-              <Box
-                className="egg"
+                className="step"
                 component="div"
                 sx={{
-                  width: "70px",
-                  position: "relative",
-                  display: "flex",
-                  justifyContent: "flex-start",
-                  alignItems: "center",
-                  ml: 2,
+                    display: "grid",
+                    gridTemplateColumns: 'auto 1fr',
+                    gridGap: {
+                        xs: '1rem',
+                        sm: '2rem',
+                        md: '4rem',
+                        lg: "4rem",
+                    },
+                    // justifyContent: "flex-start",
+                    alignItems: "center",
                 }}
-              >
-                <img src={props.URL} alt="" />
-                <Typography
-                  className="egg-title"
-                  variant="h6"
-                  sx={{
-                    color: "#282020",
-                    fontWeight: 900,
-                    position: "absolute",
-                    top: "50%",
-                    left: "50%",
-                    transform: "translate(-50%,-50%)",
-                      fontFamily: `'Paytone One', sans-serif`,
-                  }}
-                  component="div"
-                  gutterBottom
+            >
+                <Box
+                    className="content-left"
+                    component="div"
+                    sx={{position: "relative"}}
                 >
-                  {props.title}
-                </Typography>
-              </Box>
+                    <Box
+                        className="egg"
+                        component="div"
+                        sx={{
+                            width: "70px",
+                            position: "relative",
+                            display: "flex",
+                            justifyContent: "flex-start",
+                            alignItems: "center",
+                            ml: 2,
+                        }}
+                    >
+                        <img src={props.URL} alt=""/>
+                        <Typography
+                            className="egg-title"
+                            variant="h6"
+                            sx={{
+                                color: "#282020",
+                                fontWeight: 900,
+                                position: "absolute",
+                                top: "50%",
+                                left: "50%",
+                                transform: "translate(-50%,-50%)",
+                                fontFamily: `'Paytone One', sans-serif`,
+                            }}
+                            component="div"
+                            gutterBottom
+                        >
+                            {props.title}
+                        </Typography>
+                    </Box>
+                </Box>
+                <Box className="content-right" component="div" >
+                    <Typography
+                        variant="h5"
+                        sx={{
+                            fontWeight: "700", color: "#DDCFC3", mb: 1,
+                            fontFamily: `'Paytone One', sans-serif`,
+                            letterSpacing: 1,
+                        }}
+                        component="div"
+                        gutterBottom
+                    >
+                        {props.query}
+                    </Typography>
+                    <Typography
+                        variant="body1"
+                        sx={{
+                            color: "#DDCFC3", mb: 1,
+                            fontFamily: `'Play', sans-serif`,
+                        }}
+                        component="div"
+                        gutterBottom
+                    >
+                        {props.answer}
+                    </Typography>
+                </Box>
             </Box>
-            <Box className="content-right" component="div" sx={{width:'80%'}}>
-              <Typography
-                variant="h5"
-                sx={{ fontWeight: "700", color: "#DDCFC3", mb: 1,
-                    fontFamily: `'Paytone One', sans-serif`,
-                    letterSpacing: 1,
-                }}
-                component="div"
-                gutterBottom
-              >
-                {props.query}
-              </Typography>
-              <Typography
-                variant="body1"
-                sx={{ color: "#DDCFC3", mb: 1,
-                    fontFamily: `'Play', sans-serif`,
-                }}
-                component="div"
-                gutterBottom
-              >
-                {props.answer}
-              </Typography>
-            </Box>
-          </Box>
         </Box>
     )
 }
